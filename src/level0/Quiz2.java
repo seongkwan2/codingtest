27fdb277f804310a320d0b3e2df4525567357e9a
seongkwan2 <zaq3195@naver.com>
2023-11-13 19:18:15 +0900

    코딩테스트1일차

---
+++ src/level0/Quiz2.java
@@ -0,0 +1,35 @@
+package level0;
+
+import java.util.Scanner;
+
+/*
+문제 : a와 b출력하기
+
+문제 설명
+정수 a와 b가 주어집니다. 각 수를 입력받아 입출력 예와 같은 형식으로 출력하는 코드를 작성해 보세요.
+
+제한사항
+-100,000 ≤ a, b ≤ 100,000
+
+입출력 예
+입력 #1
+4 5
+
+출력 #1
+a = 4
+b = 5
+ */
+public class Quiz2 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int a = sc.nextInt();
+		int b = sc.nextInt();
+
+		if (-100000 <= a && a <= 100000 && -100000 <= b && b <= 100000) {
+			System.out.println("a = "+a);
+			System.out.println("b = "+b);
+		}else {
+			System.out.println("입력값이 제한사항을 충족하지 않습니다.");
+		}
+	}
+}
---
+++ src/level0/Quiz5.java
@@ -0,0 +1,16 @@
+package level0;
+
+/*
+문제 : 특수문자 출력하기
+
+문제 설명
+다음과 같이 출력하도록 코드를 작성해 주세요.
+
+출력예시
+!@#$%^&*(\'"<>?:;
+ */
+public class Quiz5 {
+    public static void main(String[] args) {
+        System.out.println("!@#$%^&*(\\'\"<>?:;");
+    }
+}
